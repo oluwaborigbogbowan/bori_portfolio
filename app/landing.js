9089e89d942d2ef0da5060c9e03af4ee556192f8
oluwaborigbogbowan <isaacibigbemi@gmail.com>
2024-03-20 01:43:21 +0100

    margin bottom

--- app/landing.js
+++ app/landing.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 function Landing() {
   return (
-    <section className='flex items-center pt-10 md:pt-0 mb-6 flex-col md:flex-row mt-5 md:h-[90vh] h-screen bg-coding w-full bg-cover bg-opacity-60 bg-black'>
+    <section className='flex items-center pt-10 md:pt-0 mb-6 flex-col md:flex-row mt-5 h-[90vh]  bg-coding w-full bg-cover bg-opacity-60 bg-black'>
       <div className='md:w-[60%] pr-5'>
         <p className='text-2xl font-semibold'>Hello &#128522;,</p>
           <p  className='text-4xl font-semibold'>I'm <span className='text-[#AAFF00]'>Bori</span>.</p>
